6dead99e774e48999c425a1d075a14e5b56b8dac
Rugtert <rutgerklomp@gmail.com>
2021-10-23 15:08:47 +0200

    it works agian

--- pallets/upticket/src/lib.rs
+++ pallets/upticket/src/lib.rs
@@ -10,9 +10,9 @@ pub mod pallet {
 	use super::*;
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
+	use scale_info::TypeInfo;
 	use sp_std::str;
 	use sp_std::vec::Vec;
-	use scale_info::TypeInfo;
 	// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
@@ -22,9 +22,9 @@ pub mod pallet {
 
 	pub type HappeningIndex = u128;
 	pub type EventPrice = u32;
-	
-	type HappeningInfoOf = HappeningInfo<HappeningIndex,EventPrice>;
+	pub type MaxSellPrice = u32;
 
+	type HappeningInfoOf = HappeningInfo<HappeningIndex, EventPrice>;
 	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo, Default)]
 	#[scale_info(skip_type_params(T))]
 	pub struct HappeningInfo<HappeningIndex, EventPrice> {
@@ -37,8 +37,17 @@ pub mod pallet {
 	pub struct TicketInfo {
 		happeningid: HappeningIndex,
 		price: EventPrice,
+		// sellable: bool,
+		// max_sell_price: MaxSellPrice,
 	}
-	type AccountIdOf<T> = <T as frame_system::Config>::AccountId;
+
+	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo, Default)]
+	#[scale_info(skip_type_params(T))]
+	pub struct AccountInfo {
+		backend_userid: Vec<u8>,
+		organisation: Vec<u8>,
+	}
+
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
@@ -48,12 +57,16 @@ pub mod pallet {
 	#[pallet::getter(fn happenings)]
 	pub(super) type Happenings<T: Config> =
 		StorageMap<_, Blake2_128Concat, HappeningIndex, HappeningInfoOf, ValueQuery>;
-	
 	#[pallet::storage]
 	#[pallet::getter(fn tickets)]
 	pub(super) type Tickets<T: Config> =
-		StorageMap<_, Blake2_128Concat, (HappeningIndex, AccountIdOf<T>), TicketInfo, ValueQuery>;
-	
+		StorageMap<_, Blake2_128Concat, (HappeningIndex, T::AccountId, Vec<u128>), TicketInfo, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn accounts)]
+	pub(super) type Accounts<T: Config> =
+		StorageMap<_, Blake2_128Concat, (T::AccountId, Vec<u8>), AccountInfo, ValueQuery>;
+
 	#[pallet::storage]
 	#[pallet::getter(fn fund_count)]
 	/// The total number of events that have been created
@@ -96,20 +109,15 @@ pub mod pallet {
 		pub fn create_event(
 			origin: OriginFor<T>,
 			price: u32,
+			event_index: Vec<u8>,
 		) -> DispatchResult {
 			ensure_signed(origin)?;
 			let index = <HappeningCount<T>>::get();
 			<HappeningCount<T>>::put(index + 1);
-			<Happenings<T>>::insert(index, HappeningInfo {
-				id: index,
-				price,
-			});
+			<Happenings<T>>::insert(index, HappeningInfo { id: index, price });
 			log::info!("{:?}", price);
-			
-			Self::deposit_event(Event::EventCreated(HappeningInfo {
-				id: index,
-				price,
-			}));
+
+			Self::deposit_event(Event::EventCreated(HappeningInfo { id: index, price }));
 			Ok(())
 		}
 		#[pallet::weight(0_000)]
@@ -120,25 +128,20 @@ pub mod pallet {
 		}
 
 		#[pallet::weight(10_000)]
-		pub fn buy_ticket(
-			origin: OriginFor<T>,
-			event_index: u128
-			) -> DispatchResult {
+		pub fn buy_ticket(origin: OriginFor<T>, event_index: u128, user_id: Vec<u128> ) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
+
 			let hap = <Happenings<T>>::get(event_index);
-			let tick = TicketInfo {
-				happeningid: hap.id,
-				price: hap.price,
-			};
-			<Tickets<T>>::insert((&hap.id, &sender), &tick);
+			let tick = TicketInfo { happeningid: hap.id, price: hap.price };
+			<Tickets<T>>::insert((&hap.id, &sender, user_id ), &tick);
 			Self::deposit_event(Event::TicketBought(tick));
 			Ok(())
 		}
 
 		#[pallet::weight(0_000)]
-		pub fn get_ticket(origin: OriginFor<T>, index: u128) -> DispatchResult {
+		pub fn get_ticket(origin: OriginFor<T>, index: u128, user_id: Vec<u128>) -> DispatchResult {
 			let sender = ensure_signed(origin)?;
-			let tick = <Tickets<T>>::get((index,&sender));
+			let tick = <Tickets<T>>::get((index, &sender, user_id));
 			Self::deposit_event(Event::TicketResult(tick));
 			Ok(())
 		}
